a52f8e60537c15fbc67bea167324867ea7bb1a61
AyxanM085 <Aykhankhm@code.edu.az>
2024-05-17 16:24:11 +0400

    Add files via upload

---
+++ task 2/task 2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string sentence = "Hello World";
+                PrintInitials(sentence);
+            }
+
+            static void PrintInitials(string input)
+            {
+                string[] words = input.Split(' ');
+
+                foreach (string word in words)
+                {
+                    if (!string.IsNullOrEmpty(word))
+                    {
+                        Console.Write(word[0] + " ");
+                    }
+                }
+            }
+        }
+    }
+
+}
+    
