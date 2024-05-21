881ca11ac7b9507f491391cf691e5a1fc8fcd5a3
AyxanM085 <Aykhankhm@code.edu.az>
2024-05-21 22:24:44 +0400

    Add files via upload

---
+++ 21.05 Task1/CAR.cs
@@ -0,0 +1,44 @@
+﻿namespace _21._05_Task1
+{
+    
+    
+        internal class Car
+        {
+
+
+            public string Mercedes;
+            public string E350;
+            public double CurrentFuel;
+            public double FuelFor1Km;
+            public double Millage;
+
+            public Car(string brend, string model, double currentFuel, double fuelFor1Km)
+            {
+                Mercedes = brend;
+                E350 = model;
+                CurrentFuel = currentFuel;
+                FuelFor1Km = fuelFor1Km;
+                Millage = 89345;
+            }
+
+            public void Drive(int km)
+            {
+                double requiredFuel = km * FuelFor1Km;
+
+                if (CurrentFuel >= requiredFuel)
+                {
+                    Millage += km;
+                    CurrentFuel -= requiredFuel;
+                    Console.WriteLine($"{km} have traveled kilometers. remaining fuel: {CurrentFuel} Litr.");
+                }
+                else
+                {
+                    Console.WriteLine("You don't have enough fuel! Try traveling less distance.");
+                }
+            }
+        }
+
+    }
+
+
+
---
+++ 21.05 Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _21._05_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                static void Main(string[] args)
+                {
+                    {
+                        Car araba = new Car("Mercedes", "E350", 50.0, 0.6);
+
+                        araba.Drive(30);
+                        araba.Drive(50);
+                        araba.Drive(10);
+
+                    }
+                }
+            }
+        }
+
+    }
+    }
+
+    
+
+
