474495977d601a6edaa866f9020cc4a6d14e6536
AyxanM085 <Aykhankhm@code.edu.az>
2024-05-30 12:46:31 +0400

    Add files via upload

---
+++ ConsoleApp1/accaunt.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    public class accaunt
+    {
+        
+    internal interface IAccount
+        {
+            public bool PasswordChecker(string password);
+
+            public void ShowInfo();
+        }
+    }
+}
+
